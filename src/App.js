74d750b6735b3fd91cc39471c6f192240f33faa9
Mario Brown <mario_brown_ii@mckinsey.com>
2023-02-17 15:01:28 -0500

    Added assets, components, & routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
 import './index.css';
+import Home from "./routes/Home";
+import Project from "./routes/Project";
+import About from "./routes/About";
+import Contact from "./routes/Contact";
+import { Route, Routes } from "react-router-dom";
+
 
 function App() {
   return (
-    <div>
-    </div>
+    <>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Project />} />
+      <Route path="/" element={<About />} />
+      <Route path="/" element={<Contact />} />
+    </Routes>
+    </>
   );
 }
 
---
+++ src/components/HeroImg.js
@@ -0,0 +1,25 @@
+import "./HeroImgStyles.css";
+
+import React from "react";
+import IntroImg from "../assets/safar-safarov-MSN8TFhJ0is-unsplash.jpg";
+import { Link } from "react-router-dom";
+
+const HeroImg = () => {
+    return (
+        <div className="hero">
+            <div className="mask">
+                <img className="intro-img" src={IntroImg} alt="Intro-Img"/>
+            </div>
+            <div className="content">
+                <p>MARIO BROWN</p>
+                <h1>Full-stack Engineer / Salesforce Admin & Developer</h1>
+                <div>
+                    <Link to="/project" className="btn">Projects</Link>
+                    <Link to="/contact" className="btn btn-light">Contact</Link>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default HeroImg;
